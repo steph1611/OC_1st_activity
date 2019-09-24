9bc788f2c65d870611eac900b959970cb89d5d9a
steph1611 <massoustephane@gmail.com>
2019-09-24 12:22:09 +0200

    Modification du Fichier app.js pour creer l'interface initiale sans composant

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      general_counter: 0,
+      resettable_counter: 0
+    };
+  } 
+
+  resetCounter= () => this.setState( {resettable_counter: 0} );
+
+  incCounters= () => this.setState(
+    {
+      resettable_counter: this.state.resettable_counter+1,
+      general_counter: this.state.general_counter+1 
+    }
   );
+
+  decCounters= () => this.setState(
+    {
+      resettable_counter: this.state.resettable_counter-1,
+      general_counter: this.state.general_counter-1
+    }
+  );
+
+  render() {
+      console.log(this.state.resettable_counter);
+      return (
+      <div className="App">
+        <header className="App-header">
+          <div>general counter : {this.state.general_counter}</div>
+          <div>resettable counter : {this.state.resettable_counter}</div>
+          <div>
+            <button onClick={this.resetCounter}>Reset</button>
+            <button onClick={this.incCounters}>+</button>
+            <button onClick={this.decCounters}>-</button>
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
